f129de515d7191fc2e2647fe26389b529a6d59a6
Batditos <Batakovalex@mail.ru>
2023-02-18 15:50:46 +0300

    сделал БД для товаров

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    image = models.ImageField(upload_to='static/media')
+    category = models.CharField(default='', max_length=21, unique=True)
+    name = models.CharField(max_length=50, unique=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    description = models.TextField(default='')
+
+    def __str__(sefl):
+        return sefl.name
