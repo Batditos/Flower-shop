a1f20996164791ff52a6afc364cc2675289f7d08
Batditos <Batakovalex@mail.ru>
2023-02-21 17:40:56 +0300

    создал функцию для вывода товаров из БД

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from shop.models import Product
+from basket.forms import CartAddProductForm
 
 def index(request):
     context = {
-        'product': Product.objects.all()
+        'products': Product.objects.all()
     }
     return render(request, 'shop/main.html', context)
+
+def catalog(request):
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'shop/catalog.html', context)
+
+
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product,
+                                id=id,
+                                available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/catalog.html', {'product': product,
+                                                        'cart_product_form': cart_product_form})
