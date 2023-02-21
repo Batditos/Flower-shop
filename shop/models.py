f9621197eca2705e7c35caf28372d10663db22c5
Batditos <Batakovalex@mail.ru>
2023-02-21 17:40:10 +0300

    указал путь для сохраниения файлом из БД

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
-    image = models.ImageField(upload_to='static/media')
+    image = models.ImageField(upload_to='media')
     category = models.CharField(default='', max_length=21, unique=True)
     name = models.CharField(max_length=50, unique=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     description = models.TextField(default='')
 
