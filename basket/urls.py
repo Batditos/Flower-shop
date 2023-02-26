d0a3b653765387c9f0d48a1c439a60be86de0de7
Batditos <Batakovalex@mail.ru>
2023-02-26 17:41:32 +0300

    подключил 2 метода из basket

--- basket/urls.py
+++ basket/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.basket_detail, name='basket_detail'),
     path('add/<str:product_id>/', views.basket_add, name='basket_add'),
     path('remove/<str:product_id>/', views.basket_remove, name='basket_remove'),
+    path('minus/<str:product_id>/', views.minus_quantity, name='minus_quantity'),
+    path('plus/<str:product_id>/', views.plus_quantity, name='plus_quantity'),
 ]
