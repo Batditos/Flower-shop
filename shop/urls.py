2c823d2cfac1d4d4b31bcefeada5f4d9e28a5f04
Batditos <Batakovalex@mail.ru>
2023-03-12 13:23:39 +0300

    изменил имя каталога

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('catalog/', views.catalog, name='index'),
+    path('catalog/', views.catalog, name='catalog'),
 
 
 ]
