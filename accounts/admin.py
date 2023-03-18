ecb4c5da7e138e48ea408eb6536da099cfa9af14
Batditos <Batakovalex@mail.ru>
2023-03-18 12:37:52 +0300

    добавил номральный вывод данных

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
+from .models import User
 from django.contrib import admin
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'phone_number']
+    list_editable = [ 'first_name', 'phone_number']
+
+
+admin.site.register(User, ProfileAdmin)
