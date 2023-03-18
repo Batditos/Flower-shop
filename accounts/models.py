bdb69eff34d301df9f0905bf4963f3c95127f168
Batditos <Batakovalex@mail.ru>
2023-03-18 12:42:11 +0300

    расширил модель User

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.dispatch import receiver
 
-# Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(max_length=255, unique=True)
+    REQUIRED_FIELDS = ['username']
+
+    phone_number = PhoneNumberField(unique=True)
+    image_user = models.ImageField(
+        upload_to='user_image/', default='user_image/default.png')
