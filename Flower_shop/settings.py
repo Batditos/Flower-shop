5b5dcbf4b48ca1209e0a38f9c9031308bb0cb742
Batditos <Batakovalex@mail.ru>
2023-03-18 12:46:43 +0300

    зарегистрировал пакет phonenumber_field и добавил кастомную аутентификацию

--- Flower_shop/settings.py
+++ Flower_shop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
     'shop',
     'basket',
     'accounts'
@@ -131,3 +132,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
+
+AUTHENTICATION_BACKENDS = [
+    'accounts.backends.EmailAuthBackend'
+]
+PHONENUMBER_DEFAULT_REGION = "RU"
