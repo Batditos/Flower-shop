eb047dc6aaa8198230121cb5096ba2e6172a2036
Batditos <Batakovalex@mail.ru>
2023-03-18 12:40:32 +0300

    внес изминения для входа по email

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,53 +1,77 @@
-from django.forms.forms import Form
-from django.forms.fields import EmailField
+
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.contrib.auth.models import User
+
+from accounts.models import User
+from phonenumber_field.modelfields import PhoneNumberField, formfields
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class RegisterForm(forms.ModelForm):
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
 
 
 class LoginForm(forms.Form):
 
-    username = forms.CharField(label='Логин', widget=forms.TextInput(
-        attrs={'placeholder': 'Введите логин', 'class': 'username'}))
+    email = forms.CharField(label='Email', widget=forms.TextInput(
+        attrs={'placeholder': 'Введите Email', 'class': 'username'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(
         attrs={'placeholder': 'Введите пароль'}))
 
     def clean(self):
         self.cd = super(LoginForm, self).clean()
-        username = self.cd['username']
+        email = self.cd['email']
         password = self.cd['password']
-        if not User.objects.filter(username=username).exists():
-            raise forms.ValidationError(
-                'Пользователь с таким логином не зарегистрирован')
 
-        user = User.objects.get(username=username)
+        if not User.objects.filter(email=email).exists():
+            raise forms.ValidationError(
+                'Пользователь с такой почтой не зарегистрирован')
+
+        user = User.objects.get(email=email)
         if user and not user.check_password(password):
             raise forms.ValidationError('Неверный пароль')
 
 
+
+
 class CustomUserCreationForm(UserCreationForm):
-    username = forms.CharField(label='Логин', min_length=5, max_length=150)
-    # first_name = forms.CharField(label='Имя')
-    # last_name = forms.CharField(label='Фамилия')
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('first_name', 'email', 'phone_number')
+        
     email = forms.EmailField(label='Email')
+    # username = forms.CharField(label='Логин', min_length=5, max_length=150)
+    first_name = forms.CharField(label='Имя', min_length=2,)
+    # last_name = forms.CharField(label='Фамилия')
+    phone_number = formfields.PhoneNumberField()
+
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(
         label='Повторите пароль', widget=forms.PasswordInput)
 
     def username_clean(self):
-        username = self.cleaned_data['username'].lower()
-        new = User.objects.filter(username=username)
+        email = self.cleaned_data['email'].lower()
+        new = User.objects.filter(email=email)
         if new.count():
             raise ValidationError("Пользователь уже существует")
-        return username
-    
+        return email
+
 
     def clean_email(self):
         email = self.cleaned_data['email'].lower()
         new = User.objects.filter(email=email)
         if new.count():
-            raise ValidationError("Пользователь с такой электронной зарегистрирован")
+            raise ValidationError(
+                "Пользователь с такой электронной зарегистрирован")
         return email
 
     def clean_password2(self):
@@ -57,14 +81,3 @@ class CustomUserCreationForm(UserCreationForm):
         if password1 and password2 and password1 != password2:
             raise ValidationError("Пароль не подходит")
         return password2
-
-    def save(self, commit=True):
-        user = User.objects.create_user(
-            self.cleaned_data['username'],
-            self.cleaned_data['email'],
-            self.cleaned_data['password1'],
-            # self.cleaned_data['first_name'],
-            # self.cleaned_data['last_name'],
-        )
-        print('сохранил')
-        return user
