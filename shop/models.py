6d0f9c612b94a69141ba3f5c8836c7fe0bee9e4c
Batditos <Batakovalex@mail.ru>
2023-04-15 18:51:41 +0300

    добваил verbose_name и get absolute url

--- shop/models.py
+++ shop/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 # from django.contrib.auth.models import User
 
+
 class Product(models.Model):
-    image = models.ImageField(upload_to='media')
-    category = models.CharField(default='', max_length=21, unique=True)
-    name = models.CharField(max_length=50, unique=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=0)
-    description = models.TextField(default='')
+    image = models.ImageField(upload_to='media', verbose_name='Фото')
+    category = models.CharField(
+        default='', max_length=21, unique=True, verbose_name='Категории')
+    name = models.CharField(max_length=50, unique=True,
+                            verbose_name='Название')
+    price = models.DecimalField(
+        max_digits=8, decimal_places=2, verbose_name='Цена')
+    quantity = models.PositiveIntegerField(
+        default=0, verbose_name='Количество')
+    description = models.TextField(default='', verbose_name='Описание')
 
     def __str__(sefl):
         return sefl.name
+
+    def get_absolute_url(self):
+        return f'/adminpanel/products/'
