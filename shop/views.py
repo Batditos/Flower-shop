51d60a178f349f095b56eabda14207bc8808dae8
Batditos <Batakovalex@mail.ru>
2023-05-21 18:42:30 +0300

    добавил поиск и просмотр товара

--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, render
 from basket.basket import Basket
 from shop.models import Product
 from basket.forms import CartAddProductForm
+from django.views.generic.detail import DetailView
+from django.db.models import Q
 
 def index(request):
     basket = Basket(request)
@@ -12,17 +14,22 @@ def index(request):
     return render(request, 'shop/main.html', context)
 
 def catalog(request):
+    search = request.POST.get('search', '')
+    if search:
+        products = Product.objects.filter(Q(name__icontains=search) | Q(category__icontains=search))
+    else:
+        products = Product.objects.all()
     context = {
-        'products': Product.objects.all()
+        'products': products
     }
     return render(request, 'shop/catalog.html', context)
 
-# def len_basket(request):
-#     basket = Basket(request)
-#     context = {
-#         "baskets": basket
-#     }
-#     return render(request, 'shop/main.html', context)
+def len_basket(request):
+    basket = Basket(request)
+    context = {
+        "baskets": basket
+    }
+    return render(request, 'shop/main.html', context)
 
 
 def product_detail(request, id):
@@ -32,3 +39,10 @@ def product_detail(request, id):
     cart_product_form = CartAddProductForm()
     return render(request, 'shop/catalog.html', {'product': product,
                                                         'cart_product_form': cart_product_form})
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'shop/detailproduct.html'
+
+
+    
