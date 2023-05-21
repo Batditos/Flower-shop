bf4bd40094b180f813f43fc6117ad06defb2b787
Batditos <Batakovalex@mail.ru>
2023-05-21 18:25:51 +0300

    добавил редактирование профиля и историю заказов

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import profile_views, user_login, logout_views, register
+from accounts.views import UpdateProfile, profile_views, user_login, logout_views, register, OrderViews
 
 app_name = "accounts"
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', user_login, name='user_login'),
     path('logout/', logout_views, name='logout_views'),
     path('register/', register, name='register'),
+    path('update/<str:pk>', UpdateProfile.as_view(), name='updateprofile'),
+    path('order/<str:pk>', OrderViews.as_view() , name='views')
 ]
