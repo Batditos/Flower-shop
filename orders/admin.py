e06e2607b9a201c71b09981151dd98e4241a7f37
Batditos <Batakovalex@mail.ru>
2023-05-21 18:39:04 +0300

    .

--- orders/admin.py
+++ orders/admin.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from .models import Order, OrderItem
 
 
-# class OrderItemInline(admin.TabularInline):
-#     model = OrderItem
-#     raw_id_fields = ['product']
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
 
 
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = ['user', 'address', 'paid',
-#                     'created', 'updated']
-#     list_filter = ['paid', 'created', 'updated']
-#     inlines = [OrderItemInline]
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','user', 'first_name', 'last_name',
+                    'phone', 'address', 'delivery_type', 'comments',
+                    'status', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    inlines = [OrderItemInline]
 
-# admin.site.register(Order, OrderAdmin)
+admin.site.register(Order, OrderAdmin)
