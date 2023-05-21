06af366daac23d017b42f3e06947e3c153d0c5b5
Batditos <Batakovalex@mail.ru>
2023-05-21 18:25:12 +0300

    добавил get url для админа

--- accounts/models.py
+++ accounts/models.py
@@ -11,10 +11,14 @@ class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     REQUIRED_FIELDS = ['username']
 
-    phone_number = PhoneNumberField(
-        unique=True,  verbose_name='Номер телефона')
-    image_user = models.ImageField(
-        upload_to='user_image/', default='user_image/default.png', verbose_name='Фото')
-
+    phone_number = PhoneNumberField(unique=True,  verbose_name='Номер телефона')
+    image_user = models.ImageField(upload_to='user_image/', default='user_image/default.png', verbose_name='Фото')
+    
     def get_absolute_url(self):
         return f'/adminpanel/users/'
+    
+    def get_absolute_url(self):
+        if self.is_staff == True:
+            return f'/adminpanel/users/'
+        else:
+            return f'/accounts/profile/'
