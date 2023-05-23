a74e9d5cd4ce1f6299b0708ec69a4598ee523033
Batditos <Batakovalex@mail.ru>
2023-05-23 17:41:31 +0300

    добавил отправку письма на почту при оформление заказа

--- orders/views.py
+++ orders/views.py
@@ -3,12 +3,9 @@ from .forms import OrderCreateForm
 from .models import Order, OrderItem
 from django.shortcuts import redirect, render
 from django.shortcuts import get_object_or_404, render
-from django.views import View
 
-from accounts.models import User
 from shop.models import Product
-
-
+from django.core.mail import send_mail
 # def main(request):
 #     context = {}
 #     return render(request, 'orders/order.html', context)
@@ -35,6 +32,13 @@ def order_create(request):
                 products = Product.objects.get(id=order_item.product.id)
                 products.quantity = products.quantity - order_item.quantity
                 products.save()
+            subject = 'Новый заказ № {}'.format(order.id)
+            message = '''Ура! Ваш букет в процессе создания! 💐\n
+Сказать спасибо было бы преуменьшением, но мы не можем обнять вас, так что… спасибо за ваш заказ! \n
+Сумма к оплате {} руб.\n
+В ближайшее время с вами свяжется менеджер для уточнения деталей\n
+Ждём вас снова ✨'''.format(cart.get_total_price())
+            send_mail(subject, message, 'flowershop01@yandex.ru',[user.email])
             cart.clear()
             return redirect('accounts:profile_views')
 
